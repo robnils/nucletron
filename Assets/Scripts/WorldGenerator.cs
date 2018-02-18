9dc99db4a605051aab2aff4ccd48dd610ab4ba19
Rob <r.nilshilliard@gmail.com>
2018-02-18 13:27:20 +0100

    always finished world building algorithm, still a little off

--- Assets/Scripts/WorldGenerator.cs
+++ Assets/Scripts/WorldGenerator.cs
@@ -9,17 +9,51 @@ public class WorldGenerator : MonoBehaviour {
 
     private const float MAX_DISTANCE_BETWEEN_PLATFORMS = 3.0f;
     private const float MIN_DISTANCE_BETWEEN_PLATFORMS = 1.0f;
+    private Vector3[] directions2d = { Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
 
     void Start () {
         platforms = new List<Transform>();
         var main = GameObject.FindGameObjectWithTag("Main");
 
         var platform = BuildStairs(main.transform, 1, 7);
-        BuildSomeDirection(platform, Vector3.forward);
+        var startingDirection = Vector3.forward;
+        BuildPath(startingDirection, 5);
+    }
+
+    private Vector3 BuildPath(Vector3 start, int depth) {
+        var current = start;
+        for (int idx = 0; idx < depth; idx++) {
+            BuildSomeDirection(platform, current);
+            var newDirection = getRandomDirection(current);            
+            current = newDirection;
+        }
+        return current;
+    }
+
+    private Vector3 getBackwardsDirectionFromPrevious(Vector3 previousDirection) {
+        if (previousDirection == Vector3.back) {
+            return Vector3.forward;
+        } else if (previousDirection == Vector3.forward) {
+            return Vector3.back;
+        } else if (previousDirection == Vector3.left) {
+            return Vector3.right;
+        } else if (previousDirection == Vector3.right) {
+            return Vector3.left;
+        } else {
+            Debug.LogError("Unexpected vector3 given," + previousDirection);
+            return Vector3.zero;
+        }
     }
 
     private Vector3 getRandomDirection(Vector3 previousDirection) {
-        return Vector3.zero;
+        var newDirection = directions2d[Random.Range(0, directions2d.Length)];
+        while (true) {
+            if (newDirection != getBackwardsDirectionFromPrevious(previousDirection)) {
+                break;
+            }
+            newDirection = directions2d[Random.Range(0, directions2d.Length)];
+        }
+        return newDirection;
     }
     
     private Transform BuildSomeDirection(Transform platform, Vector3 direction) {
